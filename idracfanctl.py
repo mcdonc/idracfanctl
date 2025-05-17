0658ec06fb5493ca4ac0f7f2367a9fd854ec5080
Chris McDonough <chrism@plope.com>
2025-05-16 23:07:57 -0400

    blackify

--- idracfanctl.py
+++ idracfanctl.py
@@ -12,37 +12,37 @@ class Fans:
     )
 
     def __init__(
-            self,
-            temp_cpu_min,
-            temp_cpu_max,
-            temp_exhaust_max,
-            fan_percent_min=10,
-            fan_percent_max=80,
-            fan_step=5,
-            hysteresis=2,
-            sleep=30,
-            disable_pcie_cooling_response=True,
-            ipmitool="ipmitool",
+        self,
+        temp_cpu_min,
+        temp_cpu_max,
+        temp_exhaust_max,
+        fan_percent_min=10,
+        fan_percent_max=80,
+        fan_step=5,
+        hysteresis=2,
+        sleep=30,
+        disable_pcie_cooling_response=True,
+        ipmitool="ipmitool",
     ):
         self.sleep = sleep
         self.last_pct = 0
         self.last_temp = 0
         self.temp_exhaust_max = temp_exhaust_max
-        self.temp_cpu_max = temp_cpu_max,
+        self.temp_cpu_max = (temp_cpu_max,)
         self.disable_pcie_cooling_response = disable_pcie_cooling_response
         self.hysteresis = hysteresis
         self.ipmitool = ipmitool
         self.fan_mode = "dynamic"
         self.cpu_curve = {}
         temp_cpu_range = temp_cpu_max - temp_cpu_min
-        for pct in range(0, fan_percent_max+fan_step, fan_step):
-            temp = temp_cpu_min  + (pct / fan_percent_max) * temp_cpu_range
+        for pct in range(0, fan_percent_max + fan_step, fan_step):
+            temp = temp_cpu_min + (pct / fan_percent_max) * temp_cpu_range
             if pct < fan_percent_min:
                 pct = fan_percent_min
             self.cpu_curve[temp] = pct
         self.logger = logging.getLogger()
         self.logger.setLevel(logging.INFO)
- 
+
     def run(self, command, **runargs):
         self.out(f"{command}", severity=logging.DEBUG)
         result = subprocess.run(
@@ -57,33 +57,33 @@ class Fans:
         return result
 
     def get_temps(self):
-        cpus = ('0Eh', '0Fh')
+        cpus = ("0Eh", "0Fh")
         exhaust = "01h"
         cmd = f"{self.ipmitool} sdr type temperature"
         result = self.run(cmd)
         cpu_temps = []
-        for line in result.stdout.split('\n'):
+        for line in result.stdout.split("\n"):
             if not line:
                 continue
-            desc, offset, _, _, deg = [ x.strip() for x in line.split('|') ]
+            desc, offset, _, _, deg = [x.strip() for x in line.split("|")]
             temp = deg.split()[0]
             if offset in cpus:
                 try:
                     cpu_temps.append(int(temp))
-                except ValueError: # cannot convert "No reading" to int
+                except ValueError:  # cannot convert "No reading" to int
                     self.out(f"Problem reading cpu {offset} temp, got {temp}")
                     cpu_temps.append(self.temp_cpu_max)
             if offset == exhaust:
                 try:
                     exhaust_temp = int(temp)
-                except ValueError: # cannot convert "No reading" to int
+                except ValueError:  # cannot convert "No reading" to int
                     self.out(f"Problem reading exhaust temp, got {temp}")
                     exhaust_temp = self.temp_exhaust_max
         temps = {
-            'cpu_max': max(cpu_temps),
-            'cpu_avg':sum(cpu_temps)/len(cpu_temps),
-            'exhaust':exhaust_temp,
-            }
+            "cpu_max": max(cpu_temps),
+            "cpu_avg": sum(cpu_temps) / len(cpu_temps),
+            "exhaust": exhaust_temp,
+        }
         return temps
 
     def set_fan_percent(self, pct):
@@ -94,8 +94,8 @@ class Fans:
     def adjust(self):
         cpu_temps = sorted(self.cpu_curve.items())
         temps = self.get_temps()
-        current_cpu_temp = temps['cpu_max']
-        current_exhaust_temp = temps['exhaust']
+        current_cpu_temp = temps["cpu_max"]
+        current_exhaust_temp = temps["exhaust"]
 
         if current_exhaust_temp > self.temp_exhaust_max:
             # panic
@@ -134,15 +134,14 @@ class Fans:
                             f"target pct {target_pct}% "
                             f"hysteresis {self.hysteresis}",
                             severity=logging.DEBUG,
-                            )
+                        )
                 break
-        else: # nobreak
+        else:  # nobreak
             self.out(
                 f"cpu temp uncontrollable, go dynamic @ {current_cpu_temp}°"
             )
             self.dynamic()
 
-            
     def manual(self):
         if self.fan_mode != "manual":
             self.out("going manual mode")
@@ -177,7 +176,8 @@ class Fans:
     def out(self, msg, severity=logging.INFO):
         self.logger.log(severity, msg)
         sys.stdout.flush()
-        
+
+
 if __name__ == "__main__":
     ap = argparse.ArgumentParser(
         description=(
@@ -201,7 +201,7 @@ if __name__ == "__main__":
         help=(
             "Max CPU temp in °C that should be allowed before revert to Dell "
             "dynamic dan control."
-            ),
+        ),
         type=int,
         default=97,
     )
@@ -210,7 +210,7 @@ if __name__ == "__main__":
         help=(
             "When exhaust temp reaches this value in °C, revert to Dell "
             "dynamic fan control."
-            ),
+        ),
         type=int,
         default=60,
     )
@@ -219,7 +219,7 @@ if __name__ == "__main__":
         help=(
             "The minimum percentage that the fans should run at when under "
             "script control."
-            ),
+        ),
         type=int,
         default=10,
     )
@@ -228,7 +228,7 @@ if __name__ == "__main__":
         help=(
             "The maxmum percentage that the fans should run at when under "
             "script control."
-            ),
+        ),
         type=int,
         default=57,
     )
@@ -252,7 +252,7 @@ if __name__ == "__main__":
         help=(
             "The number of seconds between attempts to readjust the fan speed "
             "the script will wait within the main loop."
-            ),
+        ),
         type=int,
         default=10,
     )
@@ -267,23 +267,21 @@ if __name__ == "__main__":
         default=0,
     )
     ap.add_argument(
-        "--ipmitool",
-        help="Path to ipmitool binary to use",
-        default="ipmitool"
-        )
+        "--ipmitool", help="Path to ipmitool binary to use", default="ipmitool"
+    )
     args = ap.parse_args()
     logging.basicConfig(level=logging.INFO, format="%(message)s")
     # all temps in °C
     fans = Fans(
-        temp_cpu_min = args.temp_cpu_min,
-        temp_cpu_max = args.temp_cpu_max,
-        temp_exhaust_max = args.temp_exhaust_max,
-        fan_percent_min = args.fan_percent_min,
-        fan_percent_max = args.fan_percent_max,
-        fan_step = args.fan_step,
-        hysteresis = args.hysteresis,
-        sleep = args.sleep,
-        disable_pcie_cooling_response = args.disable_pcie_cooling_response,
-        ipmitool = args.ipmitool
+        temp_cpu_min=args.temp_cpu_min,
+        temp_cpu_max=args.temp_cpu_max,
+        temp_exhaust_max=args.temp_exhaust_max,
+        fan_percent_min=args.fan_percent_min,
+        fan_percent_max=args.fan_percent_max,
+        fan_step=args.fan_step,
+        hysteresis=args.hysteresis,
+        sleep=args.sleep,
+        disable_pcie_cooling_response=args.disable_pcie_cooling_response,
+        ipmitool=args.ipmitool,
     )
     fans.control()
